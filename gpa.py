bf82dbb0e14dc508956247424ea8b91c93de5181
abraryasir2014 <abraryasir2014@gmail.com>
2025-08-25 20:15:29 +0600

    Add files via upload

---
+++ gpa.py
@@ -0,0 +1,15 @@
+a =int(input("enter your marks:"))
+if 88 <= a <=100:
+    print("A+")
+elif 70 <= a <=79:
+    print("A")
+elif 60 <= a <=69:
+    print("A-")
+elif 50 <= a <=59:
+    print("b")
+elif 40 <= a <=49:
+    print("c")
+elif 30 <= a <=39:
+    print("d")
+else:
+    print("fail")
