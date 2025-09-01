4f128eab0b651f599f1639826447af47cbe3fa3d
abraryasir2014 <abraryasir2014@gmail.com>
2025-09-01 19:34:31 +0600

    Add files via upload

---
+++ user loging.py
@@ -0,0 +1,12 @@
+print ("Login System")
+username = input("Enter your username:")
+
+if username == "coder125":
+    password = input("Enter your password")
+    if password == "ilovefootball!!!!!":
+        print("login successfull")
+    else:
+        print("incorret password,TRY AGAIN")
+        
+else:
+    print("user not found!")
